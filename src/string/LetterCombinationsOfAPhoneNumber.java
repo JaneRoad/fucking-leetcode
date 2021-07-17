db5adfa9c6c41660236b5bb9ae1f57b9fad8cc2c
卢建男 <lujiannan@dianchu.com>
2021-07-17 15:04:23 +0800

    21.07.14 21.07.15 21.07.16 21.07.17 打卡

---
+++ src/string/LetterCombinationsOfAPhoneNumber.java
@@ -0,0 +1,42 @@
+package string;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class LetterCombinationsOfAPhoneNumber {
+    public List<String> letterCombinations(String digits) {
+        List<String> combinations = new ArrayList();
+        if (digits.length() == 0) {
+            return combinations;
+        }
+        Map<Character, String> phoneMap = Map.of(
+                '2', "abc", '3', "def", '4', "ghi", '5', "jkl",
+                '6', "mno", '7', "pqrs", '8', "tuv", '9', "wxyz"
+        );
+        backtrack(combinations, phoneMap, digits, 0, new StringBuffer());
+        return combinations;
+    }
+
+    public void backtrack(List<String> combinations, Map<Character, String> phoneMap, String digits, int index, StringBuffer combination) {
+        if (index == digits.length()) {
+            combinations.add(combination.toString());
+        } else {
+            char digit = digits.charAt(index);
+            String letters = phoneMap.get(digit);
+            int lettersCount = letters.length();
+            for (int i = 0; i < lettersCount; i++) {
+                combination.append(letters.charAt(i));
+                backtrack(combinations, phoneMap, digits, index + 1, combination);
+                combination.deleteCharAt(index);
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        LetterCombinationsOfAPhoneNumber l = new LetterCombinationsOfAPhoneNumber();
+        List<String> strings = l.letterCombinations("32");
+        strings.forEach(System.out::println);
+    }
+}
