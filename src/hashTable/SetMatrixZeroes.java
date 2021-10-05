a412c597eb07be6e5a7873941956031c1672b0a3
janeroad <425999212@qq.com>
2021-10-05 23:18:02 +0800

    211001 211002 211003 211004 211005 打卡

---
+++ src/hashTable/SetMatrixZeroes.java
@@ -0,0 +1,34 @@
+package hashTable;
+
+public class SetMatrixZeroes {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        boolean flagCol0 = false;
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                flagCol0 = true;
+            }
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = matrix[0][j] = 0;
+                }
+            }
+        }
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+            if (flagCol0) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = new int[][]{{1,1,1},{1,0,1},{1,1,1}};
+        SetMatrixZeroes s = new SetMatrixZeroes();
+        s.setZeroes(arr);
+    }
+}
