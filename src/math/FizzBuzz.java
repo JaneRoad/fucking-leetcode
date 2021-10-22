9fa082ca53898046d256fb49f7502b26c6ebde78
janeroad <425999212@qq.com>
2021-10-22 23:21:40 +0800

    211011 211012 211013 211014 211015 211016 211017 211018 211019 211020 211021 211022 打卡

---
+++ src/math/FizzBuzz.java
@@ -0,0 +1,24 @@
+package math;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz {
+    public List<String> fizzBuzz(int n) {
+        List<String> answer = new ArrayList<String>();
+        for (int i = 1; i <= n; i++) {
+            StringBuffer sb = new StringBuffer();
+            if (i % 3 == 0) {
+                sb.append("Fizz");
+            }
+            if (i % 5 == 0) {
+                sb.append("Buzz");
+            }
+            if (sb.length() == 0) {
+                sb.append(i);
+            }
+            answer.add(sb.toString());
+        }
+        return answer;
+    }
+}
