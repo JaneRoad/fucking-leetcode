dd7c51e743a2b8373bb7c032ebcd4682b163cad0
JianNanLu <425999212@qq.com>
2021-12-18 11:36:14 +0800

    feat(每日一题打卡): 这周做了贪心算法、数学、图（拓扑排序）、几何、深度优先搜索这几种类型题目
    211212
    211213
    211214
    211215
    211216
    211217
    211218
    每日一题打卡

--- src/depthFirstSearch/BattleshipsInABoard.java
+++ src/depthFirstSearch/BattleshipsInABoard.java
@@ -1,4 +1,25 @@
 package depthFirstSearch;
 
 public class BattleshipsInABoard {
+    /**
+     * @Author JaneRoad
+     * @Description 419. 甲板上的战舰
+     * @Date 11:35 2021/12/18
+     * @Param 
+     * @param board
+     * @return 
+     * @return int
+     **/
+    public int countBattleships(char[][] board) {
+        int m = board.length, n = board[0].length;
+        int ans = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i > 0 && board[i - 1][j] == 'X') continue;
+                if (j > 0 && board[i][j - 1] == 'X') continue;
+                if (board[i][j] == 'X') ans++;
+            }
+        }
+        return ans;
+    }
 }
