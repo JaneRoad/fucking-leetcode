1d6b2502bb1203d9dc551fcf3f1dcce3688c1190
janeroad <425999212@qq.com>
2022-03-26 14:18:34 +0800

    feat(每日一题打卡): 20220323 20220324 20220325 20220326

---
+++ src/dynamicProgramming/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,25 @@
+package dynamicProgramming;
+
+/**
+ * 121. 买卖股票的最佳时机
+ */
+public class BestTimeToBuyAndSellStock {
+    /**
+     * 假如计划在第 i 天卖出股票，那么最大利润的差值一定是在[0, i-1] 之间选最低点买入；
+     * 所以遍历数组，依次求每个卖出时机的的最大差值，再从中取最大值。
+     * @param prices
+     * @return
+     */
+    public int maxProfit(int prices[]) {
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice) {
+                minprice = prices[i];
+            } else if (prices[i] - minprice > maxprofit) {
+                maxprofit = prices[i] - minprice;
+            }
+        }
+        return maxprofit;
+    }
+}
---
+++ src/stack/BaseballGame.java
@@ -0,0 +1,27 @@
+package stack;
+
+/**
+ * 682. 棒球比赛
+ */
+public class BaseballGame {
+    static int[] nums = new int[1010];
+    public int calPoints(String[] ops) {
+        int n = ops.length, idx = 0;
+        for (int i = 0; i < n; i++, idx++) {
+            if (ops[i].equals("+")){
+                nums[idx] = nums[idx - 1] + nums[idx - 2];
+            } else if (ops[i].equals("D")) {
+                nums[idx] = nums[idx - 1] * 2;
+            } else if (ops[i].equals("C")){
+                idx -= 2;
+            } else{
+                nums[idx] = Integer.parseInt(ops[i]);
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < idx; i++) {
+            ans += nums[i];
+        }
+        return ans;
+    }
+}
