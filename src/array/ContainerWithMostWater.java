6ad3b7aa066b2b8ecf2bd76ebe07d5daa99da5dd
janeroad <425999212@qq.com>
2022-09-01 22:01:56 +0800

    feat(接雨水、盛最多水的容器)

--- src/array/ContainerWithMostWater.java
+++ src/array/ContainerWithMostWater.java
@@ -1,19 +1,25 @@
 package array;
 
+/**
+ * 11. 盛最多水的容器
+ * @author janeroad
+ */
 public class ContainerWithMostWater {
     public int maxArea(int[] height) {
-        int l = 0, r = height.length - 1;
-        int ans = 0;
-        while (l < r) {
-            int area = Math.min(height[l], height[r]) * (r - l);
-            ans = Math.max(ans, area);
-            if (height[l] <= height[r]) {
-                ++l;
-            }
-            else {
-                --r;
+        int left = 0, right = height.length - 1;
+        int res = 0;
+
+        while (left < right) {
+            // [left, right] 之间的矩形面积
+            int curArea = Math.min(height[left], height[right]) * (right - left);
+            res = Math.max(res, curArea);
+            // 双指针技巧，移动较低的一边
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
             }
         }
-        return ans;
+        return res;
     }
 }
--- src/twoPointers/TrappingRainWater.java
+++ src/twoPointers/TrappingRainWater.java
@@ -1,21 +1,38 @@
 package twoPointers;
 
+/**
+ * 42. 接雨水
+ *
+ * @author janeroad
+ */
 public class TrappingRainWater {
+
     public int trap(int[] height) {
-        int ans = 0;
         int left = 0, right = height.length - 1;
         int leftMax = 0, rightMax = 0;
+        int res = 0;
+
+        //双指针遍历
         while (left < right) {
             leftMax = Math.max(leftMax, height[left]);
             rightMax = Math.max(rightMax, height[right]);
-            if (height[left] < height[right]) {
-                ans += leftMax - height[left];
-                ++left;
+
+            // res += min(leftMax, rightMax) - height[i]
+            if (leftMax < rightMax) {
+                res += leftMax - height[left];
+                left++;
             } else {
-                ans += rightMax - height[right];
-                --right;
+                res += rightMax - height[right];
+                right--;
             }
         }
-        return ans;
+        return res;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+        TrappingRainWater trappingRainWater = new TrappingRainWater();
+        System.out.println(trappingRainWater.trap(arr));
     }
 }
