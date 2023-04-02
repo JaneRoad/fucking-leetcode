b3bd3bd0df0431614c538cefae5e2c95c5c2755d
janeroad <425999212@qq.com>
2023-04-02 17:14:55 +0800

    feat(前缀和数组)

--- src/prefixSum/RangeSumQuery2DImmutable.java
+++ src/prefixSum/RangeSumQuery2DImmutable.java
@@ -2,15 +2,24 @@ package prefixSum;
 
 /**
  * 304. 二维区域和检索 - 矩阵不可变
+ * @author janeroad
  */
 public class RangeSumQuery2DImmutable {
-    class NumMatrix {
-        // preSum[i][j] 记录矩阵 [0, 0, i, j] 的元素和
+    static class NumMatrix {
+        /**
+         * preSum[i][j] 记录矩阵 [0, 0, i, j] 的元素和
+         */
         private int[][] preSum;
 
         public NumMatrix(int[][] matrix) {
-            int m = matrix.length, n = matrix[0].length;
-            if (m == 0 || n == 0) return;
+            int m = matrix.length;
+            if (m == 0) {
+                return;
+            }
+            int n = matrix[0].length;
+            if (n == 0) {
+                return;
+            }
             // 构造前缀和矩阵
             preSum = new int[m + 1][n + 1];
             for (int i = 1; i <= m; i++) {
@@ -21,10 +30,23 @@ public class RangeSumQuery2DImmutable {
             }
         }
 
-        // 计算子矩阵 [x1, y1, x2, y2] 的元素和
+        /**
+         * 计算子矩阵 [x1, y1, x2, y2] 的元素和
+         * @param x1
+         * @param y1
+         * @param x2
+         * @param y2
+         * @return
+         */
         public int sumRegion(int x1, int y1, int x2, int y2) {
             // 目标矩阵之和由四个相邻矩阵运算获得
             return preSum[x2+1][y2+1] - preSum[x1][y2+1] - preSum[x2+1][y1] + preSum[x1][y1];
         }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{3, 0, 1, 4, 2}, {5, 6, 3, 2, 1}, {1, 2, 0, 1, 5}, {4, 1, 0, 1, 7}, {1, 0, 3, 0, 5}};
+        NumMatrix numMatrix = new NumMatrix(matrix);
+        System.out.println(numMatrix.sumRegion(2, 1, 4, 3));
     }
 }
--- src/prefixSum/RangeSumQueryImmutable.java
+++ src/prefixSum/RangeSumQueryImmutable.java
@@ -2,21 +2,35 @@ package prefixSum;
 
 /**
  * 303. 区域和检索 - 数组不可变
+ * @author janeroad
  */
 public class RangeSumQueryImmutable {
 
     class NumArray {
-
-
+        /**
+         * 前缀和数组
+         */
         private int[] preSum;
 
+        /**
+         * 输入一个数组，构造前缀和 NumArray
+         * @param nums
+         */
         public NumArray(int[] nums) {
+            // preSum[0] = 0，便于计算累加和
             preSum = new int[nums.length + 1];
-            for (int i = 1; i < preSum.length; i++){
+            // 计算 nums 的累加和
+            for (int i = 1; i < preSum.length; i++) {
                 preSum[i] = preSum[i - 1] + nums[i - 1];
             }
         }
 
+        /**
+         * 查询闭区间 [left, right] 的累加和
+         * @param left
+         * @param right
+         * @return
+         */
         public int sumRange(int left, int right) {
             return preSum[right + 1] - preSum[left];
         }
