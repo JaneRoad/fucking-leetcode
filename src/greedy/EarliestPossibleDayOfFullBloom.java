5f5357ae9691e5d7d12c1bbd1a5e8d06714abed0
janeroad <425999212@qq.com>
2023-09-30 12:22:04 +0800

    feat(2136. 全部开花的最早一天)

---
+++ src/greedy/EarliestPossibleDayOfFullBloom.java
@@ -0,0 +1,25 @@
+package greedy;
+
+import java.util.Arrays;
+
+/**
+ * 2136. 全部开花的最早一天
+ * @author janeroad
+ */
+public class EarliestPossibleDayOfFullBloom {
+
+    public int earliestFullBloom(int[] plantTime, int[] growTime) {
+        int n = plantTime.length;
+        var id = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            id[i] = i;
+        }
+        Arrays.sort(id, (i, j) -> growTime[j] - growTime[i]);
+        int ans = 0, days = 0;
+        for (int i : id) {
+            days += plantTime[i]; // 累加播种天数
+            ans = Math.max(ans, days + growTime[i]); // 再加上生长天数，就是这个种子的开花时间
+        }
+        return ans;
+    }
+}
